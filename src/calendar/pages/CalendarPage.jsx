92fe91bcb09e46c838acbd3ed514e6d67e2930d0
PabloFC <pablofc_68@hotmail.com>
2024-12-24 00:06:27 +0100

    React big calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,48 @@
 import Navbar from "../components/Navbar";
 
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours, parse, startOfWeek, getDay, format } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "Mi cumpleaños",
+    notes: "me tiene que comprar un regalo",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Pablo",
+    },
+  },
+];
+
 const CalendarPage = () => {
   return (
     <div>
       <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+      />
     </div>
   );
 };
