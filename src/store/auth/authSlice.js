606e70dd97d2672ca6a6cf873557e7ff93dc5f46
PabloFC <pablofc_68@hotmail.com>
2025-02-22 21:28:42 +0100

    authSlice

---
+++ src/store/auth/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    status: "checking",
+    user: {},
+    erorMessage: undefined,
+  },
+  reducers: {
+    onChecking: (state) => {
+      state.status = "checking";
+      state.user = {};
+      state.erorMessage = undefined;
+    },
+    onLogin: (state, { payload }) => {
+      state.status = "authenticated";
+      state.user = payload;
+      state.erorMessage = undefined;
+    },
+  },
+});
+// Action creators are generated for each case reducer function
+export const { onChecking, onLogin } = authSlice.actions;
