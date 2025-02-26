021ac8226de5ee073902fb704a9335b24d04b2e0
PabloFC <pablofc_68@hotmail.com>
2025-02-26 12:10:04 +0100

    despachar acciones respectivas

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -5,20 +5,29 @@ export const authSlice = createSlice({
   initialState: {
     status: "checking",
     user: {},
-    erorMessage: undefined,
+    errorMessage: undefined,
   },
   reducers: {
     onChecking: (state) => {
       state.status = "checking";
       state.user = {};
-      state.erorMessage = undefined;
+      state.errorMessage = undefined;
     },
     onLogin: (state, { payload }) => {
       state.status = "authenticated";
       state.user = payload;
-      state.erorMessage = undefined;
+      state.errorMessage = undefined;
+    },
+    onLogout: (state, { payload }) => {
+      state.status = "not-authenticated";
+      state.user = {};
+      state.errorMessage = payload;
+    },
+    clearErrorMessage: (state) => {
+      state.errorMessage = undefined;
     },
   },
 });
 // Action creators are generated for each case reducer function
-export const { onChecking, onLogin } = authSlice.actions;
+export const { onChecking, onLogin, onLogout, clearErrorMessage } =
+  authSlice.actions;
